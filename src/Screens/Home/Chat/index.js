fb86181141a7fcfd9c54204084c05407767e5739
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-10-04 13:36:54 -0700

    [Implement] - Sent Message Work Into DB

--- src/Screens/Home/Chat/index.js
+++ src/Screens/Home/Chat/index.js
@@ -17,9 +17,14 @@ import {
 // PACKAGES
 import {withNavigation} from 'react-navigation';
 import Search from 'react-native-vector-icons/Ionicons';
+import firebase from 'react-native-firebase';
 
 // FILES
 import styles from './style';
+
+const currentUserId = firebase.auth().currentUser.uid;
+
+const db = firebase.database();
 
 class Chat extends Component {
   constructor(props) {
@@ -27,56 +32,56 @@ class Chat extends Component {
     this.state = {
       msg: '',
       messages: [
-        {
-          id: 1,
-          sent: true,
-          msg: 'Lorem ipsum dolor',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
-        },
-        {
-          id: 2,
-          sent: true,
-          msg: 'sit amet, consectetuer',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
-        },
-        {
-          id: 3,
-          sent: false,
-          msg: 'adipiscing elit. Aenean ',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
-        },
-        {
-          id: 4,
-          sent: true,
-          msg: 'commodo ligula eget dolor.',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
-        },
-        {
-          id: 5,
-          sent: false,
-          msg:
-            'Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
-        },
-        {
-          id: 6,
-          sent: true,
-          msg:
-            'nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
-        },
-        {
-          id: 7,
-          sent: false,
-          msg: 'rhoncus ut, imperdiet',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
-        },
-        {
-          id: 8,
-          sent: false,
-          msg: 'a, venenatis vitae',
-          image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
-        },
+        // {
+        //   id: 1,
+        //   sent: true,
+        //   msg: 'Lorem ipsum dolor',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
+        // },
+        // {
+        //   id: 2,
+        //   sent: true,
+        //   msg: 'sit amet, consectetuer',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
+        // },
+        // {
+        //   id: 3,
+        //   sent: false,
+        //   msg: 'adipiscing elit. Aenean ',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
+        // },
+        // {
+        //   id: 4,
+        //   sent: true,
+        //   msg: 'commodo ligula eget dolor.',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
+        // },
+        // {
+        //   id: 5,
+        //   sent: false,
+        //   msg:
+        //     'Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
+        // },
+        // {
+        //   id: 6,
+        //   sent: true,
+        //   msg:
+        //     'nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
+        // },
+        // {
+        //   id: 7,
+        //   sent: false,
+        //   msg: 'rhoncus ut, imperdiet',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
+        // },
+        // {
+        //   id: 8,
+        //   sent: false,
+        //   msg: 'a, venenatis vitae',
+        //   image: 'https://www.bootdey.com/img/Content/avatar/avatar6.png',
+        // },
       ],
       receiverName: '',
       receiverId: '',
@@ -95,11 +100,27 @@ class Chat extends Component {
       color: 'white',
     },
     headerLeft: (
-      <TouchableOpacity style={{paddingLeft: 10}} onPress={() => navigation.goBack()}>
+      <TouchableOpacity
+        style={{paddingLeft: 10}}
+        onPress={() => navigation.goBack()}>
         <Search name="md-arrow-back" color="white" size={25} />
       </TouchableOpacity>
     ),
   });
+
+  componentDidMount() {
+    const {id} = this.props.navigation.state.params.userInfo;
+    const that = this;
+    let messages = [];
+
+    db.ref('Messages/').on('child_added', snapshot => {
+      messages.push(snapshot.val());
+
+      that.setState({messages});
+    });
+
+    this.setState({receiverId: id});
+  }
 
   reply() {
     var messages = this.state.messages;
@@ -113,28 +134,35 @@ class Chat extends Component {
   }
 
   send() {
-    if (this.state.msg.length > 0) {
-      var messages = this.state.messages;
-      messages.push({
-        id: Math.floor(Math.random() * 99999999999999999 + 1),
-        sent: true,
-        msg: this.state.msg,
-        image: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
-      });
-      this.setState({messages: messages});
-      setTimeout(() => {
-        this.reply();
-      }, 2000);
+    const {msg, receiverId} = this.state;
+    const currentTime = new Date().getTime();
+
+    if (msg) {
+      const msgObj = {
+        senderId: currentUserId,
+        receiverId,
+        message: msg,
+        sendTime: currentTime,
+        seen: false,
+      };
+
+      db
+        .ref('Messages/')
+        .push(msgObj)
+        .then(success => this.setState({msg: ''}))
+        .catch(error => console.log(error, 'something went wrong!!!'));
+    } else {
+      alert('Can not be Empty!');
     }
   }
 
   _renderItem = ({item}) => {
-    if (item.sent === false) {
+    if (item.seen === false) {
       return (
         <View style={styles.eachMsg}>
-          <Image source={{uri: item.image}} style={styles.userPic} />
+          <Image source={{uri: "https://www.bootdey.com/img/Content/avatar/avatar1.png"}} style={styles.userPic} />
           <View style={styles.msgBlock}>
-            <Text style={styles.msgTxt}>{item.msg}</Text>
+            <Text style={styles.msgTxt}>{item.message}</Text>
           </View>
         </View>
       );
@@ -142,24 +170,24 @@ class Chat extends Component {
       return (
         <View style={styles.rightMsg}>
           <View style={styles.rightBlock}>
-            <Text style={styles.rightTxt}>{item.msg}</Text>
+            <Text style={styles.rightTxt}>{item.message}</Text>
           </View>
-          <Image source={{uri: item.image}} style={styles.userPic} />
+          <Image source={{uri: "https://www.bootdey.com/img/Content/avatar/avatar6.png"}} style={styles.userPic} />
         </View>
       );
     }
   };
 
   render() {
+    const {messages} = this.state;
+
     return (
       <View style={{flex: 1}}>
         <FlatList
           style={styles.list}
           extraData={this.state}
-          data={this.state.messages}
-          keyExtractor={item => {
-            return item.id;
-          }}
+          data={messages}
+          keyExtractor={(item, index) => index.toString()}
           renderItem={this.renderItem}
         />
         <View style={styles.footer}>
@@ -168,11 +196,11 @@ class Chat extends Component {
               style={styles.inputs}
               placeholder="Write a message..."
               underlineColorAndroid="transparent"
-              onChangeText={name_address => this.setState({name_address})}
+              onChangeText={msg => this.setState({msg})}
             />
           </View>
 
-          <TouchableOpacity style={styles.btnSend}>
+          <TouchableOpacity style={styles.btnSend} onPress={() => this.send()}>
             <Image
               source={{
                 uri: 'https://png.icons8.com/small/75/ffffff/filled-sent.png',
